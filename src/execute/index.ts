067c6de3b478692902761c70dfd5c00a518b2548
sosukesuzuki <aosukeke@gmail.com>
2021-01-30 20:29:21 +0900

    Fix

--- src/execute/index.ts
+++ src/execute/index.ts
@@ -9,20 +9,6 @@ import * as configuration from "../configuration";
 const getTargetRepositoryPath = (targetRepositoryName: string) =>
   path.join(configuration.targetRepositoriesPath, targetRepositoryName);
 
-//  リポジトリセットアップ手順
-//    prNumber の場合
-//      gh をインストールする
-//      gh pr checkout prNumber する
-//      yarn install
-//    repositoryAndRef の場合
-//      git remote add sosukesuzuki sosukesuzuki/prettier
-//      git fetch sosukesuzuki
-//      git checkout ref
-//      yarn install
-//    version の場合
-//      git checkout tags/1.1.0 する
-//      yarn install
-
 export async function execute({
   alternativePrettier,
   originalPrettier,
@@ -35,11 +21,7 @@ export async function execute({
       getPrettyHeadCommitHash(getTargetRepositoryPath(targetRepositoryName))
     )
   );
-  //  original
-  //    まず original を リポジトリセットアップ手順 に従ってセットアップする
-  //    original を各 projects に対して実行する
-  //    各プロジェクトで変更差分をコミット(コミットメッセージは"Changed by 何かしら識別できる情報")
-  // Setup originalVersionPrettier
+
   await setupPrettierRepository(originalPrettier);
   await Promise.all(
     targetRepositoryNames.map(async (targetRepositoryName) => {
