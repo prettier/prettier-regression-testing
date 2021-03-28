85d3b3aa4eda9140e897a2449d180d3353ad8bbf
sosukesuzuki <aosukeke@gmail.com>
2021-03-29 01:53:42 +0900

    Run yarn install if another package is required to run prettier

--- src/execute/run-prettier.ts
+++ src/execute/run-prettier.ts
@@ -1,6 +1,7 @@
 import execa from "execa";
 import path from "path";
 import { projects } from "../projects";
+import * as yarn from "../tools/yarn";
 
 export async function runPrettier(
   prettierRepositoryPath: string,
@@ -23,8 +24,20 @@ export async function runPrettier(
   if (ignore) {
     args.push("--ignore-path", ignore);
   }
-  await execa(prettierRepositoryBinPath, args, {
-    cwd: repositoryPath,
-    shell: true,
-  });
+  try {
+    await execa(prettierRepositoryBinPath, args, {
+      cwd: repositoryPath,
+      shell: true,
+    });
+  } catch (error) {
+    // if another packages is required to run Prettier
+    // e.g. excalidraw: https://github.com/excalidraw/excalidraw/blob/a21db08cae608692d9525fff97f109fb24fec20c/package.json#L83
+    if (error.message.includes("Cannot find module")) {
+      await yarn.install(repositoryPath);
+      await execa(prettierRepositoryBinPath, args, {
+        cwd: repositoryPath,
+        shell: true,
+      });
+    }
+  }
 }
