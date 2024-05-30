4c99ff2bebd3d61245f77a2f1eef0384dd71748d
Sosuke Suzuki <aosukeke@gmail.com>
2024-05-30 15:49:48 +0900

    run prettier after updating projects.json

--- scripts/update-projects-json.mjs
+++ scripts/update-projects-json.mjs
@@ -1,3 +1,4 @@
+import * as prettier from "prettier";
 import fs from "fs/promises";
 import path from "path";
 import url from "url";
@@ -42,7 +43,7 @@ async function updateProjectsJsonFile() {
   }
   await fs.writeFile(
     projectsJsonPath,
-    JSON.stringify(newProjects, null, 2) + "\n",
+    await prettier.format(JSON.stringify(newProjects)),
   );
 }
 
