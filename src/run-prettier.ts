2b2c09515c8d33231493de5e52cc8a98d60f3be7
fisker <lionkay@gmail.com>
2025-12-16 05:12:11 +0800

    Remove useless head in diff

--- src/run-prettier.ts
+++ src/run-prettier.ts
@@ -104,7 +104,7 @@ export async function runPrettier(
 
   const fileLinkPrefix = `https://github.com/${repository.repository}/tree/${repository.commit}/`;
 
-  const { stdout: diff } = await spawn(
+  let { stdout: diff } = await spawn(
     "git",
     [
       "diff",
@@ -115,6 +115,21 @@ export async function runPrettier(
     ],
     { cwd: directory },
   );
+
+  // Remove the useless head
+  const lines = diff.split("\n");
+  for (let lineIndex = lines.length - 1; lineIndex > 0; lineIndex--) {
+    if (
+      lines[lineIndex].startsWith("diff --git Original|") &&
+      lines[lineIndex + 1].startsWith("index ") &&
+      lines[lineIndex + 2].startsWith("--- Original|http://github.com/") &&
+      lines[lineIndex + 3].startsWith("+++ Alternative|")
+    ) {
+      lines.splice(lineIndex, 2);
+    }
+  }
+
+  diff = lines.join("\n");
 
   if (diff) {
     console.log(diff);
