7dd5ed63ed1387e85de77a3c3493813703c93b79
fisker <lionkay@gmail.com>
2025-12-21 08:11:57 +0800

    Fix diff

--- src/run-prettier.ts
+++ src/run-prettier.ts
@@ -108,8 +108,8 @@ export async function runPrettier(
     "git",
     [
       "diff",
-      alternative.version.kind,
       original.version.kind,
+      alternative.version.kind,
       `--src-prefix=${original.version.kind}|${fileLinkPrefix}`,
       `--dst-prefix=${alternative.version.kind}|`,
     ],
